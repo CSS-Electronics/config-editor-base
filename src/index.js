bce245dc6b8b46512749e99bd3b1b91a5afee8e8
mfcss <mf@csselectronics.com>
2026-07-15 09:07:30 +0200

    Expose loadFile for use in config-editor-tools

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import * as editorActions from './editorBase/actions'
 import OBDTool from './editorBaseTools/OBDTool'
 import FilterBuilderTool from './editorBaseTools/FilterBuilderTool'
 import { computeConfigDelta } from './editorBase/configDelta'
+// loadFile resolves an embedded dist schema/uischema by "name | DeviceType"
+// (e.g. "schema-01.09.json | CANedge2 GNSS"). Exported so tools such as the
+// migration tool can load the official dist schema for a target revision.
+import { loadFile } from './editorBase/utils'
 
 export {
   EditorSection,
@@ -11,5 +15,6 @@ export {
   editorActions,
   OBDTool,
   FilterBuilderTool,
-  computeConfigDelta
+  computeConfigDelta,
+  loadFile
 }
